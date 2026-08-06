cb8b12b4c6862224b08b03d9f2df5da3f9e71f70
sophiasolo <774052646@qq.com>
2026-08-06 14:22:43 +0800

    强化套餐说明: 必须融入文案不可忽略

--- js/app_v8.js
+++ js/app_v8.js
@@ -1390,7 +1390,7 @@ function generateCopyAI(){
     '【排版】每行≤20字，不允许断词换行。像朋友群发消息，不机械不模板。\\n\\n'+
     '【推送日】'+dayName+'\n'+
     '【产品名（必须原样使用，不可改！）】'+product+'\\n'+
-    (CopyConfig.comboNote?'【套餐详情】'+CopyConfig.comboNote+'\\n':'')+
+    (CopyConfig.comboNote?'【套餐详情（必须融入文案，不可忽略！】'+CopyConfig.comboNote+'\\n':'')+
     '【价格要求】'+priceRule+'\n'+
     '【配送费要求】'+deliveryRule+'\n'+
     '【方向】'+(direction==='auto'?'根据推送日自由选择':direction)+'\n\n'+
